e182b72161b14087bdcfefda255013c05136cc63
Chuck Smith <chuck@eclecticsaddlebag.com>
2019-08-03 17:09:16 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,8 @@
 function basicTeenager(age) {
-  if (age > 13 && age < 19) {
+  if (age >= 13 && age <= 19) {
     console.log("You are a teenager!")
   }
 }
-basicTeenager(17)
 
 function teenager(age) {
   if (age > 13 && age < 19) {
